014032f3a9b65804983dcf6407908b5ffe7bfbf9
irensmile <curkan.ira@gmail.com>
2024-03-31 13:15:28 -0400

    controlers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,25 @@
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = (req, res) => {
+  return contactsService.listContacts();
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = (req, res) => {
+  const { id } = req.params;
+  return contactsService.getContactById(id);
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = (req, res) => {
+  return contactsService.removeContact(Id);
+};
 
-export const createContact = (req, res) => {};
+export const createContact = (req, res) => {
+  const { name, email, phone } = req.body;
+  return contactsService.addContact(name, email, phone);
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = (req, res) => {
+  const { id } = req.params;
+  const { name, email, phone } = req.body;
+  return contactsService.updateContact(id, name, email, phone);
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,103 @@
+import { promises as fs } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
+import { nanoid } from "nanoid";
+
+// __dirname is not available with type=module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const contactsPath = path.join(__dirname, "db", "contacts.json"); // Шлях до файлу з контактами
+
+export async function listContacts() {
+  try {
+    // Зчитуємо дані з файлу
+    const data = await fs.readFile(contactsPath);
+    // Перетворюємо рядок JSON у масив об'єктів
+    const contacts = JSON.parse(data);
+    // Повертаємо масив контактів
+    return contacts;
+  } catch (error) {
+    // Обробляємо помилку, якщо файл не знайдено або сталася інша помилка при зчитуванні
+    console.error("Failed to read contacts:", error);
+    return []; // Повертаємо порожній масив у випадку помилки
+  }
+}
+
+export async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const listElements = JSON.parse(data);
+    const foundElement = listElements.find(
+      (element) => element.id === contactId
+    );
+    return foundElement || null;
+  } catch (error) {
+    console.error("Failed to find a contact:", error);
+    return null;
+  }
+}
+
+export async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    let contacts = JSON.parse(data);
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    // Якщо контакт не знайдений, повертаємо null
+    if (index === -1) return null;
+
+    //Знайшли елемент для видаленя з індексом index
+
+    const removedContact = contacts.splice(index, 1)[0];
+
+    // Записуємо зміни у файл
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    // Повертаємо видалений контакт
+    return removedContact;
+  } catch (error) {
+    // Обробляємо помилку, якщо файл не знайдено або сталася інша помилка при зчитуванні
+    console.error("Failed to remove contact:", error);
+    return null; // Повертаємо null у випадку помилки
+  }
+}
+
+export async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    let contacts = JSON.parse(data);
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error("Unable to add a new contact:", error);
+    return null;
+  }
+}
+
+export async function updateContact(id, name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    let contacts = JSON.parse(data);
+
+    const foundElement = listElements.find((element) => element.id === id);
+    if (foundElement === null) return null;
+
+    foundElement.name = name;
+    foundElement.email = email;
+    foundElement.phone = phone;
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return foundElement;
+  } catch (error) {
+    console.error("Unable to add a new contact:", error);
+    return null;
+  }
+}
