bff380c6585eb6495bb87e0b3e5da230f509fcb8
irensmile <curkan.ira@gmail.com>
2024-04-01 13:03:52 -0400

    some refactoring

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,7 +1,3 @@
-import {
-  createContactSchema,
-  updateContactSchema,
-} from "../schemas/contactsSchemas.js";
 import contactsService from "../services/contactsServices.js";
 
 export const getAllContacts = async (req, res) => {
@@ -23,45 +19,21 @@ export const deleteContact = async (req, res) => {
 };
 
 export const createContact = async (req, res, next) => {
-  const validationResult = createContactSchema.validate(req.body);
-  try {
-    if (validationResult.error) {
-      const errorMessages = validationResult.error.details.map(
-        (detail) => detail.message
-      );
-      throw Error(errorMessages.join(", "));
-    }
+  const { name, email, phone } = req.body;
 
-    const { name, email, phone } = req.body;
-
-    const newContact = await contactsService.addContact(name, email, phone);
-    res.status(201).json(newContact);
-  } catch (error) {
-    next({ status: 400, message: error.message });
-  }
+  const newContact = await contactsService.addContact(name, email, phone);
+  res.status(201).json(newContact);
 };
 
-export const updateContact = async (req, res, next) => {
-  const validationResult = updateContactSchema.validate(req.body);
-  try {
-    if (validationResult.error) {
-      const errorMessages = validationResult.error.details.map(
-        (detail) => detail.message
-      );
-      throw Error(errorMessages.join(", "));
-    }
-
-    const { id } = req.params;
-    const { name, email, phone } = req.body;
-    const updatedContact = await contactsService.updateContact(
-      id,
-      name,
-      email,
-      phone
-    );
-    if (updatedContact === null) res.status(404).json({ message: "Not found" });
-    else res.status(200).json(updatedContact);
-  } catch (error) {
-    next({ status: 400, message: error.message });
-  }
+export const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const { name, email, phone } = req.body;
+  const updatedContact = await contactsService.updateContact(
+    id,
+    name,
+    email,
+    phone
+  );
+  if (updatedContact === null) res.status(404).json({ message: "Not found" });
+  else res.status(200).json(updatedContact);
 };
