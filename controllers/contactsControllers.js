66b10e65d27c8d9d7147ef8a5d99e4d08051e167
irensmile <curkan.ira@gmail.com>
2024-04-15 11:05:17 -0400

    add owner

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,39 +1,46 @@
 import contactsService from "../services/contactsServices.js";
 
 export const getAllContacts = async (req, res) => {
-  console.log("User:", req.user);
-  res.status(200).json(await contactsService.listContacts());
+  res.status(200).json(await contactsService.listContacts(req.user));
 };
 
 export const getOneContact = async (req, res) => {
   const { id } = req.params;
-  const contact = await contactsService.getContactById(id);
+  const contact = await contactsService.getContactById(id, req.user);
   if (contact === null) res.status(404).json({ message: "Not found" });
   else res.status(200).json(contact);
 };
 
 export const deleteContact = async (req, res) => {
   const { id } = req.params;
-  const contact = await contactsService.removeContact(id);
+  const contact = await contactsService.removeContact(id, req.user);
   if (contact === null) res.status(404).json({ message: "Not found" });
   else res.json(contact).status(200);
 };
 
 export const createContact = async (req, res, next) => {
-  const newContact = await contactsService.addContact(req.body);
+  const newContact = await contactsService.addContact(req.body, req.user);
   res.status(201).json(newContact);
 };
 
 export const updateContact = async (req, res) => {
   const { id } = req.params;
-  const updatedContact = await contactsService.updateContact(id, req.body);
+  const updatedContact = await contactsService.updateContact(
+    id,
+    req.body,
+    req.user
+  );
   if (updatedContact === null) res.status(404).json({ message: "Not found" });
   else res.status(200).json(updatedContact);
 };
 
 export const updateStatusContact = async (req, res) => {
   const { id } = req.params;
-  const updatedContact = await contactsService.updateContact(id, req.body);
+  const updatedContact = await contactsService.updateContact(
+    id,
+    req.body,
+    req.user
+  );
   if (updatedContact === null) res.status(404).json({ message: "Not found" });
   else res.status(200).json(updatedContact);
 };
--- helpers/verifyAuth.js
+++ helpers/verifyAuth.js
@@ -2,29 +2,22 @@ import { mongooseUserModel } from "../schemas/userSchemas.js";
 import jwt from "jsonwebtoken";
 
 export const verifyAuth = async (req, res, next) => {
-  console.log("Headers:", req.headers);
   const { authorization = "" } = req.headers;
 
-  console.log("authorization:", authorization);
   const [bearer, token] = authorization.split(" ");
-
-  console.log("Bearer: ", bearer, token);
 
   if (bearer !== "Bearer" || !token) {
     res.status(401).json({ message: "Not authorized" });
   }
-  console.log("Bearer: ", bearer, token);
+
   try {
     const payload = jwt.verify(token, process.env.SECRET_WORD);
-    console.log("payload:", payload);
 
     const foundUser = await mongooseUserModel.findById(payload.id);
-    console.log("User: ", foundUser);
 
     if (!foundUser) {
       res.status(401).json({ message: "Not authorized" });
     }
-    console.log("USER");
     if (foundUser.token != token) {
       res.status(401).json({ message: "Not authorized" });
     }
