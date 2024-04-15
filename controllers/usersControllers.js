ef6fcf01b26562d276b203d1f0493b44b32ec166
irensmile <curkan.ira@gmail.com>
2024-04-15 09:11:38 -0400

    Registration

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,3 +1,23 @@
+import bcrypt from "bcrypt";
+import { mongooseUserModel } from "../schemas/userSchemas.js";
+
 export const register = async (req, res) => {
-  res.status(200).json({});
+  if (mongooseUserModel.findOne({ email: req.body.email }) !== null) {
+    res.status(409).json({
+      message: "Email in use",
+    });
+  }
+
+  bcrypt.hash(req.body.password, 10, async function (err, hash) {
+    const user = await mongooseUserModel.create({
+      password: hash,
+      email: req.body.email,
+    });
+    res.status(201).json({
+      user: {
+        email: user.email,
+        subscription: user.subscription,
+      },
+    });
+  });
 };
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { register } from "../controllers/usersControllers.js";
+import validateBody from "../helpers/validateBody.js";
+import { registerUserSchema } from "../schemas/userSchemas.js";
 
 const usersRouter = express.Router();
-usersRouter.post("/users/register", register);
+usersRouter.post("/users/register", validateBody(registerUserSchema), register);
 
 export default usersRouter;
